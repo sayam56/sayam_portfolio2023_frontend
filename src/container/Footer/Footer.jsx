f87c0882c4e9d558e3c2a734cc18d6c28fad6f16
ali-iktider-sayam <aisayam.sayam@gmail.com>
2022-12-30 02:21:21 +0600

    fixed footer jsx

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 
 import { images } from '../../constants';
 import { AppWrap, MotionWrap } from '../../wrapper';
